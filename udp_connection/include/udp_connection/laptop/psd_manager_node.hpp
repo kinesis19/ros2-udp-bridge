6fd070669925f359d7f83ec27cf926e9ccd5a05a
kinesis19 <lordkinesis@naver.com>
2024-11-22 20:18:47 +0900

    :sparkles: Feat: Subscribe /stm/psd_adc_value topic
    STM32의 PSD(ADC) value 서브스크라이브 및 로그 출력 구현 완료.

--- udp_connection/include/udp_connection/laptop/psd_manager_node.hpp
+++ udp_connection/include/udp_connection/laptop/psd_manager_node.hpp
@@ -3,6 +3,8 @@
 
 #include <QThread>
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/int32.hpp>
 
 class PsdManagerNode : public QThread
 {
@@ -18,7 +20,18 @@ protected:
 
 private:
     rclcpp::Node::SharedPtr node;
+
+    // ========== [Psd-Adc-Value 서브스크라이브] ==========
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub_stm32_psd_adc_right_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub_stm32_psd_adc_front_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub_stm32_psd_adc_left_;
+
     bool initialized_; // 초기화 상태 확인 변수
+    
+    // ========== [STM32 PSD Value Callback Method] ==========
+    void psdRightCallback(const std_msgs::msg::Int32::SharedPtr msg);
+    void psdFrontCallback(const std_msgs::msg::Int32::SharedPtr msg);
+    void psdLeftCallback(const std_msgs::msg::Int32::SharedPtr msg);
 };
 
 #endif // UDP_CONNECTION_PSD_MANAGER_NODE_HPP
--- udp_connection/src/laptop/psd_manager_node.cpp
+++ udp_connection/src/laptop/psd_manager_node.cpp
@@ -7,6 +7,11 @@ PsdManagerNode::PsdManagerNode()
     RCLCPP_INFO(node->get_logger(), "psd_manager_node 초기화 완료");
 
     initialized_ = true;
+
+    // ========== [STM32의 PSD(ADC) Value 서브스크라이브] ==========
+    sub_stm32_psd_adc_right_ = node->create_subscription<std_msgs::msg::Int32>("/stm32/psd_adc_value_right", 10, std::bind(&PsdManagerNode::psdRightCallback, this, std::placeholders::_1));
+    sub_stm32_psd_adc_front_ = node->create_subscription<std_msgs::msg::Int32>("/stm32/psd_adc_value_front", 10, std::bind(&PsdManagerNode::psdFrontCallback, this, std::placeholders::_1));
+    sub_stm32_psd_adc_left_ = node->create_subscription<std_msgs::msg::Int32>("/stm32/psd_adc_value_left", 10, std::bind(&PsdManagerNode::psdLeftCallback, this, std::placeholders::_1));
 }
 
 PsdManagerNode::~PsdManagerNode()
@@ -30,4 +35,17 @@ void PsdManagerNode::run()
 bool PsdManagerNode::isInitialized() const
 {
     return initialized_; // 초기화 상태 반환
+}
+
+// ========== [STM32 PSD Value Callback Method] ==========
+void PsdManagerNode::psdRightCallback(const std_msgs::msg::Int32::SharedPtr msg) {
+    RCLCPP_INFO(node->get_logger(), "PSD Right: %d", msg->data);
+}
+
+void PsdManagerNode::psdFrontCallback(const std_msgs::msg::Int32::SharedPtr msg) {
+    RCLCPP_INFO(node->get_logger(), "PSD Front: %d", msg->data);
+}
+
+void PsdManagerNode::psdLeftCallback(const std_msgs::msg::Int32::SharedPtr msg) {
+    RCLCPP_INFO(node->get_logger(), "PSD Left: %d", msg->data);
 }
